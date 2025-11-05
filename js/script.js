394f727f9111fb105c62670ad021653ffc84f333
tmiguel78 <tmiguel78@yahoo.es>
2025-11-05 10:20:08 +0100

    ejercicio hecho

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+boton.addEventListener('click', () => {
+    const elemento = prompt('Agrega un elemento');
+    const elementoLista = document.createElement('li');
+    lista.appendChild(elementoLista);
+    elementoLista.textContent = elemento;
+})
